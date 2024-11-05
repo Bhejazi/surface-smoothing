c20964bece18d6f5feb03b70ed51164d17530f7e
Bardia Hejazi <bardia.hejazi@gmail.com>
2024-11-05 13:43:38 +0100

    fixed to mesh surfaces with openings

--- mesh_smoothing_function.py
+++ mesh_smoothing_function.py
@@ -5,48 +5,24 @@ Created on Wed Nov 29 16:11:46 2023
 @author: bhejazi
 """
 import numpy as np
-from stl import mesh
 import open3d as o3d
-
 #%%
 def mesh_smoothing(data_type, file_path, poisson_depth, save_path, save_file_name):
-    
     #%%          
     def read_stl(file_path):
         # Read the STL file
-        
-        mesh_data = mesh.Mesh.from_file(file_path)
-    
+        mesh_data = o3d.io.read_triangle_mesh(file_path)
         # Get the 3D vertex coordinates
-        vertices = mesh_data.vectors.reshape((-1, 3))
-    
-        # Create a dictionary to map unique vertex coordinates to their indices
-        vertex_dict = {}
-        unique_vertices = []
-        vertex_labels = []
-    
-        for vertex in vertices:
-            vertex_tuple = tuple(vertex)
-            if vertex_tuple not in vertex_dict:
-                vertex_dict[vertex_tuple] = len(unique_vertices)
-                unique_vertices.append(vertex)
-                vertex_labels.append(vertex_dict[vertex_tuple])
-    
-        unique_vertices = np.array(unique_vertices)
-    
-        # Get the connectivity list (faces as indices of the vertex points)
-        connectivity = []
-        num_vertices_per_face = 3  # Assuming the STL file contains only triangular faces
-    
-        for i in range(0, len(vertices), num_vertices_per_face):
-            face = [vertex_dict[tuple(vertices[j])] for j in range(i, i + num_vertices_per_face)]
-            connectivity.append(face)
-    
-        return unique_vertices, np.array(connectivity)
+        vertices = np.asarray(mesh_data.vertices)
+        vert_norms = np.asarray(mesh_data.vertex_normals)
+        connectivity = np.asarray(mesh_data.triangles)
         
+        return vertices, vert_norms, connectivity
     #%%
     if data_type == "Mesh":
-        pnts, connections = read_stl(file_path)
+        
+        print("Reducing mesh points...")
+        pnts, norms, connections = read_stl(file_path)
         
         pnts_list = np.arange(0, pnts.shape[0])
         odd_pnts = np.array([])
@@ -59,31 +35,39 @@ def mesh_smoothing(data_type, file_path, poisson_depth, save_path, save_file_nam
         while len(pnts_list) > 0:
         
             counter += 1
-            if np.mod(counter, 1000) == 0:
+            if np.mod(counter, 2000) == 0:
                 print("# of points left to categorize: " + str(len(pnts_list)))
                 
             rnd_pnt = np.random.choice(pnts_list)
-            row, col = np.where(connections == rnd_pnt)
-            connection_list = np.unique(connections[row, :])
-                  
-            if connection_list.shape[0] == 1:
-                connection_list = connection_list.reshape(-1, 1)
+            rnd_vert = pnts[rnd_pnt, :]
+            rnd_vert_list = np.array(np.where((pnts[:,0] == rnd_vert[0]) & (pnts[:,1] == rnd_vert[1]) & (pnts[:,2] == rnd_vert[2])))
+            connection_list = np.array([])
+            
+            odd_pnts = np.append(odd_pnts, rnd_vert_list)
+            
+            for i in range(rnd_vert_list.shape[1]):
+                row, col = np.where(connections == rnd_vert_list[0,i])
+                connection_list = np.append(connection_list, connections[row, :])
+                
+            connection_list = np.setdiff1d(connection_list, rnd_vert_list)
+            connection_list = connection_list.astype(int)
             
             for j in range(connection_list.shape[0]):
-                if connection_list[j] == rnd_pnt:
-                    odd_pnts = np.append(odd_pnts, rnd_pnt)
-                else:
-                    even_pnts = np.append(even_pnts, connection_list[j])
-            
-                pnts_list = np.setdiff1d(pnts_list, np.union1d(odd_pnts, even_pnts))
+                even_pnt = pnts[connection_list[j], :]
+                even_pnt_list = np.array(np.where((pnts[:,0] == even_pnt[0]) & (pnts[:,1] == even_pnt[1]) & (pnts[:,2] == even_pnt[2])))
+                even_pnts = np.append(even_pnts, even_pnt_list)
+
+            pnts_list = np.setdiff1d(pnts_list, np.union1d(odd_pnts, even_pnts))
                     
         even_pnts = np.unique(even_pnts)
             
         pnts_reduced = pnts[even_pnts.astype(int), :]
-        np.save(f"{save_path}\\{save_file_name}.npy", pnts_reduced)
-    #pnts_deleted = pnts[odd_pnts.astype(int), :]
+        norms_reduced = norms[even_pnts.astype(int), :]
+        np.save(f"{save_path}\\{save_file_name}.npy", pnts_reduced, norms_reduced)
+        #pnts_deleted = pnts[odd_pnts.astype(int), :]
     elif data_type == "Point cloud":
-        pnts_reduced = np.load(file_path) 
+        pnts_reduced = np.load(file_path)
+        norms_reduced = np.load(file_path) 
     else:
         print("Invalid input data type.")           
     #%%
@@ -95,56 +79,28 @@ def mesh_smoothing(data_type, file_path, poisson_depth, save_path, save_file_nam
         
     #%%
     point_cloud = pnts_reduced
-        
     pcd  = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(point_cloud[:,:3])
         
     # visualize the point cloud
     # o3d.visualization.draw_geometries([pcd],window_name="Point Cloud", width=800, height=800)
-        
     #%% Estimate point normals needed for meshing
     pcd.normals = o3d.utility.Vector3dVector(np.zeros((1, 3)))  # invalidate existing normals
-    
-    pcd.estimate_normals()
-    #o3d.visualization.draw_geometries([pcd], point_show_normal=True)
-    
-    pcd.orient_normals_consistent_tangent_plane(100)
-    
-    print("See if normals are calculated correctly and pointing outward, close figures to continue.", flush = True)
-        
-    #%%
-    o3d.visualization.draw_geometries([pcd], point_show_normal=True, width=800, height=800, 
-                                      window_name="Normals")
-        
-    user_input = input("Are normals pointing outward? (y/n): ")
-    
-    # Check the user's response and execute the corresponding command
-    if user_input.lower() == "y":
-        print("Continuing to meshing.")
-    elif user_input.lower() == "n":
-        temp = np.array(pcd.normals)
-        temp = -temp
-        pcd.normals=o3d.utility.Vector3dVector(temp)
-        o3d.visualization.draw_geometries([pcd], point_show_normal=True, width=800, height=800, 
-                                          window_name="Normals after correction")
-        print("Continuing to meshing.")
-    else:
-        print("Invalid input. Please enter 'y' or 'n'.")
-        
+    pcd.normals = o3d.utility.Vector3dVector(norms_reduced)    #o3d.visualization.draw_geometries([pcd], point_show_normal=True)
+                
+    # o3d.visualization.draw_geometries([pcd], point_show_normal=True, width=800, height=800, window_name="Normals")
     #%% Poisson reconstruction, has to have normals
     ## good for water tight meshes
-    poisson_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcd, depth=poisson_depth, width=0, 
-                                                                                 scale=1.1, linear_fit=False)
+    print("Meshing reduced points...")
+    poisson_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcd, depth=poisson_depth, width=0, scale=1.1, linear_fit=False)
         
     poisson_mesh = poisson_mesh[0]
     poisson_mesh.compute_vertex_normals()
     poisson_mesh.paint_uniform_color([1, 0.706, 0])
-    #o3d.visualization.draw_geometries([poisson_mesh], window_name="Mesh Poisson")
         
-    with o3d.utility.VerbosityContextManager(
-            o3d.utility.VerbosityLevel.Debug) as cm:
-        triangle_clusters, cluster_n_triangles, cluster_area = (
-            poisson_mesh.cluster_connected_triangles())
+    #with o3d.utility.VerbosityContextManager(
+    #        o3d.utility.VerbosityLevel.Debug) as cm:
+    triangle_clusters, cluster_n_triangles, cluster_area = (poisson_mesh.cluster_connected_triangles())
     triangle_clusters = np.asarray(triangle_clusters)
     cluster_n_triangles = np.asarray(cluster_n_triangles)
     cluster_area = np.asarray(cluster_area)
@@ -157,21 +113,8 @@ def mesh_smoothing(data_type, file_path, poisson_depth, save_path, save_file_nam
     poisson_mesh.remove_duplicated_triangles()
     poisson_mesh.remove_duplicated_vertices()
     poisson_mesh.remove_non_manifold_edges()
-    # assert poisson_mesh.is_watertight(), "Mesh has holes"
-        
-    # poisson_mesh_t = o3d.t.geometry.TriangleMesh.from_legacy(poisson_mesh) # to tensor
-    # filled_poisson_mesh = poisson_mesh_t.fill_holes()
-    # assert filled_poisson_mesh.to_legacy().is_watertight(), "Mesh still has holes"
-        
-    # filled_poisson_mesh_final = filled_poisson_mesh.to_legacy()
-        
-    # print(filled_poisson_mesh_final)
-    # o3d.visualization.draw_geometries([filled_poisson_mesh_final], window_name="Mesh Poisson removed small triangles", 
-    #                                   width=800, height=800)
-    print(poisson_mesh)
-    o3d.visualization.draw_geometries([poisson_mesh], window_name="Mesh with Poisson method", 
-                                      width=800, height=800)
-        
-    #%% Export mesh
+
+    #%% Export mesh  
     o3d.io.write_triangle_mesh(f"{save_path}\\{save_file_name}.stl", poisson_mesh)
-        
+    # Visualize mesh
+    o3d.visualization.draw_geometries([poisson_mesh], window_name="Mesh with Poisson method", width=800, height=800)
